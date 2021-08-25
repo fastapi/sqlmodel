9ff2b1430e05711a0429d6039c95acec6fce6aad
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-08-25 17:21:54 +0200

    Raise exception if no matching SQLAlchemy type

---
+++ tests/test_missing_type.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+import pytest
+from sqlmodel import Field, SQLModel, create_engine
+
+
+def test_missing_sql_type():
+    class CustomType:
+        @classmethod
+        def __get_validators__(cls):
+            yield cls.validate
+
+        @classmethod
+        def validate(cls, v):
+            return v
+            
+    with pytest.raises(ValueError):
+        class Item(SQLModel, table=True):
+            id: Optional[int] = Field(default=None, primary_key=True)
+            item: CustomType
