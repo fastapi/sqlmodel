155c6178cd18671c8046c27c22ed580a9e4b1fb9
Sebastián Ramírez <tiangolo@gmail.com>
2021-12-28 11:48:03 +0100

    ✨ Document indexes and make them opt-in (#205)

--- docs_src/tutorial/many_to_many/tutorial001.py
+++ docs_src/tutorial/many_to_many/tutorial001.py
@@ -14,7 +14,7 @@ class HeroTeamLink(SQLModel, table=True):
 
 class Team(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(index=True)
     headquarters: str
 
     heroes: List["Hero"] = Relationship(back_populates="teams", link_model=HeroTeamLink)
@@ -22,9 +22,9 @@ class Team(SQLModel, table=True):
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(index=True)
     secret_name: str
-    age: Optional[int] = None
+    age: Optional[int] = Field(default=None, index=True)
 
     teams: List[Team] = Relationship(back_populates="heroes", link_model=HeroTeamLink)
 
