cc9350079924872291fab286d244d7e685c67a12
Malthe Karbo <malthe@resights.dk>
2022-06-04 21:11:41 +0200

    Add fix #322: sqlalchemy 1.4.36 breaks sqlmodel

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -369,6 +369,8 @@ class SQLModelMetaclass(ModelMetaclass, DeclarativeMeta):
                     relationship_to, *rel_args, **rel_kwargs
                 )
                 dict_used[rel_name] = rel_value
+                # From https://github.com/tiangolo/sqlmodel/pull/322
+                setattr(cls, rel_name, rel_value)  # Fix #315
             DeclarativeMeta.__init__(cls, classname, bases, dict_used, **kw)
         else:
             ModelMetaclass.__init__(cls, classname, bases, dict_, **kw)
