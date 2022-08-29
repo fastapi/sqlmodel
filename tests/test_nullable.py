e22617ebe13a064ef4d213c2a624fd4f0047700b
Benjamin Rapaport <br@getallstreet.com>
2022-08-29 07:31:30 -0400

    Columns that are defined with an `Optional` type but a non `nullable` `Field` are inserting into the database as nullable. This was introduced in https://github.com/tiangolo/sqlmodel/commit/9830ee0d8991ffc068ffb72ccead2427c84e58ee#r82434170 and described in #420.
    Example:
    ```
    required_field: Optional[str] = Field(nullable=False)
    ```
    
    - Added a test to confirm the regression
    - Fixed test by re-ordering the code that determines if a field is nullable.
    
    Co-authored-by: Jonas Krüger Svensson <jonas-ks@hotmail.com>, building off his PR: https://github.com/tiangolo/sqlmodel/pull/423
    
    add coverage reports
    
    run code coverage
    
    Add more checks to the test for the regression.
    
    Co-authored-by: Jonas Krüger Svensson <jonas-ks@hotmail.com>
    
    Fix comment on test
    
    Co-authored-by: Jonas Krüger Svensson <jonas-ks@hotmail.com>
    
    Formatting, comments
    Co-authored-by: Jonas Krüger Svensson <jonas-ks@hotmail.com>
    
    Remove newline
    
    Co-authored-by: Jonas Krüger Svensson jonas-ks@hotmail.com
    
    remove comment
    
    simplify test
    
    rename variable
    
    add missing assert

--- tests/test_nullable.py
+++ tests/test_nullable.py
@@ -1,36 +1,33 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel, create_engine
+import pytest
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 def test_nullable_fields_set_properly(clear_sqlmodel, caplog):
     class Hero(SQLModel, table=True):
-        # Uses Field
-        nullable_integer_primary_key: Optional[int] = Field(
+        primary_key: Optional[int] = Field(
             default=None,
             primary_key=True,
         )
-        non_nullable_integer_with_field: int = Field(default=..., nullable=False)
-
-        # Does not use Field
-        nullable_integer: Optional[int] = None
-        non_nullable_integer: int
-
-        # Uses Field
-        non_nullable_optional_string_with_field: Optional[str] = Field(
-            default=..., nullable=False
+        required_value: str
+        optional_non_nullable: Optional[str] = Field(
+            nullable=False,
         )
-        non_nullable_optional_string_with_field_no_default_set: Optional[str] = Field(
-            nullable=False
+        optional_default_ellipses_non_nullable: Optional[str] = Field(
+            default=...,
+            nullable=False,
         )
-        non_nullable_optional_string_with_field_with_default: Optional[str] = Field(
-            default=None, nullable=False
+        optional_default_none_non_nullable: Optional[str] = Field(
+            default=None,
+            nullable=False,
         )
-        non_nullable_string_with_field: str = Field(default=..., nullable=False)
-
-        # Does not use Field
-        nullable_optional_string: Optional[str] = Field(default=None, nullable=True)
-        non_nullable_string: str
+        default_ellipses_non_nullable: str = Field(default=..., nullable=False)
+        optional_default_none_nullable: Optional[str] = Field(
+            default=None,
+            nullable=True,
+        )
 
     engine = create_engine("sqlite://", echo=True)
     SQLModel.metadata.create_all(engine)
@@ -38,26 +35,18 @@ def test_nullable_fields_set_properly(clear_sqlmodel, caplog):
     create_table_log = [
         message for message in caplog.messages if "CREATE TABLE hero" in message
     ][0]
-    assert "\n\tnullable_integer_primary_key INTEGER NOT NULL," in create_table_log
-    assert "\n\tnullable_integer INTEGER," in create_table_log
-    assert "\n\tnon_nullable_integer INTEGER NOT NULL," in create_table_log
-    assert "\n\tnon_nullable_integer_with_field INTEGER NOT NULL," in create_table_log
-
-    assert "\n\tnullable_optional_string VARCHAR," in create_table_log
+    assert "\n\tprimary_key INTEGER NOT NULL," in create_table_log
+    assert "\n\trequired_value VARCHAR NOT NULL," in create_table_log
+    assert "\n\toptional_non_nullable VARCHAR NOT NULL," in create_table_log
     assert (
-        "\n\tnon_nullable_optional_string_with_field VARCHAR NOT NULL,"
+        "\n\toptional_default_ellipses_non_nullable VARCHAR NOT NULL,"
         in create_table_log
     )
     assert (
-        "\n\tnon_nullable_optional_string_with_field_no_default_set VARCHAR NOT NULL,"
-        in create_table_log
+        "\n\toptional_default_none_non_nullable VARCHAR NOT NULL," in create_table_log
     )
-    assert (
-        "\n\tnon_nullable_optional_string_with_field_with_default VARCHAR NOT NULL,"
-        in create_table_log
-    )
-    assert "\n\tnon_nullable_string VARCHAR NOT NULL," in create_table_log
-    assert "\n\tnon_nullable_string_with_field VARCHAR NOT NULL," in create_table_log
+    assert "\n\tdefault_ellipses_non_nullable VARCHAR NOT NULL," in create_table_log
+    assert "\n\toptional_default_none_nullable VARCHAR," in create_table_log
 
 
 # Test for regression in https://github.com/tiangolo/sqlmodel/issues/420
@@ -78,3 +67,11 @@ def test_non_nullable_optional_field_with_no_default_set(clear_sqlmodel, caplog)
     ][0]
     assert "\n\tnullable_integer_primary_key INTEGER NOT NULL," in create_table_log
     assert "\n\toptional_non_nullable_no_default VARCHAR NOT NULL," in create_table_log
+
+    # We can create a hero with `None` set for the optional non-nullable field
+    hero = Hero(nullable_integer_primary_key=123, optional_non_nullable_no_default=None)
+    # But we cannot commit it.
+    with Session(engine) as session:
+        session.add(hero)
+        with pytest.raises(IntegrityError):
+            session.commit()
