0024745ef94bcfb3b98a6ebeffe904f894ccddfa
David Danier <danier@team23.de>
2022-08-29 17:20:10 +0200

    Add failing test for test_get_sqlachemy_type()

---
+++ tests/test_get_sqlachemy_type.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict, List
+from unittest.mock import MagicMock
+
+import pytest
+from pydantic.fields import ModelField
+
+from sqlmodel.main import get_sqlachemy_type
+
+
+@pytest.mark.parametrize(
+    "input_type",
+    [
+        List[str],
+        Dict[str, Any],
+    ],
+)
+def test_non_type_does_not_break(input_type: type) -> None:
+    model_field_mock = MagicMock(ModelField, type_=input_type)
+    with pytest.raises(ValueError):
+        get_sqlachemy_type(model_field_mock)
