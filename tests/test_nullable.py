4ca8a40a6e2ddf14a392452b1daae5268b4eda38
Benjamin Rapaport <br@getallstreet.com>
2022-08-29 10:54:33 -0400

    - Update tests/test_nullable.py to exercise the failing condition of   setting an `Optional` type but passing `nullable=True` to the `Field`   definition - Fix the bug

--- tests/test_nullable.py
+++ tests/test_nullable.py
@@ -1,20 +1,63 @@
 from typing import Optional
 
 import pytest
-from sqlalchemy.exc import InterfaceError
-from sqlalchemy.orm import Session
-from sqlmodel import Field, SQLModel, create_engine
+from sqlalchemy.exc import IntegrityError
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 def test_nullable_fields_set_properly(clear_sqlmodel, caplog):
     class Hero(SQLModel, table=True):
-        nullable_integer_primary_key: Optional[int] = Field(
-            default=None, primary_key=True
+        primary_key: Optional[int] = Field(
+            default=None,
+            primary_key=True,
         )
-        nullable_optional_string: Optional[str] = Field(default=None, nullable=True)
-        nullable_integer: Optional[int] = None
-        not_null_string: str = Field(default=..., nullable=False)
-        not_null_integer: int
+        required_value: str
+        optional_non_nullable: Optional[str] = Field(
+            nullable=False,
+        )
+        optional_default_ellipses_non_nullable: Optional[str] = Field(
+            default=...,
+            nullable=False,
+        )
+        optional_default_none_non_nullable: Optional[str] = Field(
+            default=None,
+            nullable=False,
+        )
+        default_ellipses_non_nullable: str = Field(default=..., nullable=False)
+        optional_default_none_nullable: Optional[str] = Field(
+            default=None,
+            nullable=True,
+        )
+
+    engine = create_engine("sqlite://", echo=True)
+    SQLModel.metadata.create_all(engine)
+
+    create_table_log = [
+        message for message in caplog.messages if "CREATE TABLE hero" in message
+    ][0]
+    assert "\n\tprimary_key INTEGER NOT NULL," in create_table_log
+    assert "\n\trequired_value VARCHAR NOT NULL," in create_table_log
+    assert "\n\toptional_non_nullable VARCHAR NOT NULL," in create_table_log
+    assert (
+        "\n\toptional_default_ellipses_non_nullable VARCHAR NOT NULL,"
+        in create_table_log
+    )
+    assert (
+        "\n\toptional_default_none_non_nullable VARCHAR NOT NULL," in create_table_log
+    )
+    assert "\n\tdefault_ellipses_non_nullable VARCHAR NOT NULL," in create_table_log
+    assert "\n\toptional_default_none_nullable VARCHAR," in create_table_log
+
+
+# Test for regression in https://github.com/tiangolo/sqlmodel/issues/420
+def test_non_nullable_optional_field_with_no_default_set(clear_sqlmodel, caplog):
+    class Hero(SQLModel, table=True):
+        nullable_integer_primary_key: Optional[int] = Field(
+            default=None,
+            primary_key=True,
+        )
+
+        optional_non_nullable_no_default: Optional[str] = Field(nullable=False)
 
     engine = create_engine("sqlite://", echo=True)
     SQLModel.metadata.create_all(engine)
@@ -23,14 +66,13 @@ def test_nullable_fields_set_properly(clear_sqlmodel, caplog):
         message for message in caplog.messages if "CREATE TABLE hero" in message
     ][0]
     assert "\n\tnullable_integer_primary_key INTEGER NOT NULL," in create_table_log
-    assert "\n\tnot_null_string VARCHAR NOT NULL," in create_table_log
-    assert "\n\tnullable_optional_string VARCHAR," in create_table_log
-    assert "\n\tnullable_integer INTEGER," in create_table_log
-    assert "\n\tnot_null_integer INTEGER NOT NULL," in create_table_log
+    assert "\n\toptional_non_nullable_no_default VARCHAR NOT NULL," in create_table_log
 
-    # Ensure we cannot create a hero with `None` set for not-nullable-fields:
-    hero = Hero(not_null_integer=None, not_null_string=None)
+    # We can create a hero with `None` set for the optional non-nullable field
+    hero = Hero(nullable_integer_primary_key=123, optional_non_nullable_no_default=None)
+    # But we cannot commit it.
     with Session(engine) as session:
         session.add(hero)
-        with pytest.raises(InterfaceError):
+        with pytest.raises(IntegrityError):
             session.commit()
+
