9e19608736cd6c78074ee527e333ad4095699842
Anthony Uphof <anthony@smartlife-labs.com>
2022-10-16 09:30:55 +1300

    test: workaround for async inspect

--- tests/test_async_tutorial/test_async_connect/test_async_create_connected_tables/test_async_tutorial001.py
+++ tests/test_async_tutorial/test_async_connect/test_async_create_connected_tables/test_async_tutorial001.py
@@ -8,35 +8,40 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 @pytest.mark.asyncio
-async def test_async_tutorial001():
+async def test_async_tutorial001()->None:
     from docs_src.tutorial_async.connect_async.create_tables_async import (
         tutorial001_async as mod,
     )
 
     mod.sqlite_url = "sqlite+aiosqlite://"
     mod.engine = create_async_engine(mod.sqlite_url)
-    meta = SQLModel.metadata
-    async_session = sessionmaker(
-        mod.engine, expire_on_commit=False, class_=AsyncSession
-    )
 
-    async with mod.engine.begin() as conn1:
-        # await conn1.run_sync(meta.drop_all)
-        await conn1.run_sync(meta.create_all)
+
     await mod.main()
 
-    # async with async_session() as session1:
-    #     async with session1.begin():
-    #         await session1.run_sync(meta.create_all)
-    # await session1.run_sync(mod.main())
 
-    # async with mod.engine.begin() as conn1:
-    #     await conn1.run_sync(mod.main())
-    # await conn1.run_sync(meta.create_all)
-    # mod.main()
 
+
+    # Following code lines are for sync implementation , see following note regarding async
+    # https://docs.sqlalchemy.org/en/14/errors.html#error-xd3s
     # insp: Inspector = inspect(mod.engine)
     # assert insp.has_table(str(mod.Hero.__tablename__))
     # assert insp.has_table(str(mod.Team.__tablename__))
 
-    assert True
+    # following is recommended workaround
+    async with mod.engine.connect() as conn:
+            tables = await conn.run_sync(
+                lambda sync_conn: inspect(sync_conn).get_table_names()
+            )
+    assert str(mod.Team.__tablename__) in tables
+    assert str(mod.Hero.__tablename__) in tables
+
+    # TODO; work out how to call insp.has_tables  with run_sync
+    # async with mod.engine.connect() as conn:
+    #     insp: Inspector = await conn.run_sync(
+    #         lambda sync_conn: inspect(sync_conn)
+    #     )
+    #     assert insp.has_table(str(mod.Hero.__tablename__))
+    #     assert insp.has_table(str(mod.Team.__tablename__))
+
+
