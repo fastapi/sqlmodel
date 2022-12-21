e8159c594761e4e2c761832eb901732ceec908df
Ezequiel Parziale <eparziale@gmail.com>
2022-12-21 19:57:36 -0300

    :test_tube: update: order generate select

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
+CHECK_JINJA=1 python scripts/generate_select.py
+
 coverage run -m pytest tests
 coverage combine
 coverage report --show-missing
 coverage html
-
-CHECK_JINJA=1 python scripts/generate_select.py
