9ae86dcde759470ab9961b85661485c36a612900
omezaldama <oscar_meza5@hotmail.com>
2023-01-28 15:43:44 -0600

    Fixed limit query parameter name typo

--- docs_src/tutorial/fastapi/app_testing/tutorial001/main.py
+++ docs_src/tutorial/fastapi/app_testing/tutorial001/main.py
@@ -66,7 +66,7 @@ def read_heroes(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, lte=100),
+    limit: int = Query(default=100, le=100),
 ):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
--- docs_src/tutorial/fastapi/relationships/tutorial001.py
+++ docs_src/tutorial/fastapi/relationships/tutorial001.py
@@ -104,7 +104,7 @@ def read_heroes(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, lte=100),
+    limit: int = Query(default=100, le=100),
 ):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
@@ -159,7 +159,7 @@ def read_teams(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, lte=100),
+    limit: int = Query(default=100, le=100),
 ):
     teams = session.exec(select(Team).offset(offset).limit(limit)).all()
     return teams
