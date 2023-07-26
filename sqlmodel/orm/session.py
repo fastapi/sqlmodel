e942e5e22c38e3d210ab16da0e778b713a3b871a
Peter Landry <peter.landry@gmail.com>
2023-07-26 14:24:01 +0200

    Execution options are not Optional in SA

--- sqlmodel/orm/session.py
+++ sqlmodel/orm/session.py
@@ -84,7 +84,7 @@ class Session(_Session):
         self,
         statement: _Executable,
         params: Optional[Union[Mapping[str, Any], Sequence[Mapping[str, Any]]]] = None,
-        execution_options: Optional[Mapping[str, Any]] = util.EMPTY_DICT,
+        execution_options: Mapping[str, Any] = util.EMPTY_DICT,
         bind_arguments: Optional[Dict[str, Any]] = None,
         _parent_execute_state: Optional[Any] = None,
         _add_event: Optional[Any] = None,
