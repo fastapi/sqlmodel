9a7f2f654e5d7d93543a1cf0a4616bec9b77592a
Sebastián Ramírez <tiangolo@gmail.com>
2023-10-23 19:17:54 +0400

    🐛 Fix Pydantic field parameter lte -> le

--- docs_src/tutorial/fastapi/teams/tutorial001.py
+++ docs_src/tutorial/fastapi/teams/tutorial001.py
@@ -95,7 +95,7 @@ def read_heroes(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, lte=100),
+    limit: int = Query(default=100, le=100),
 ):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
@@ -149,7 +149,7 @@ def read_teams(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
-    limit: int = Query(default=100, lte=100),
+    limit: int = Query(default=100, le=100),
 ):
     teams = session.exec(select(Team).offset(offset).limit(limit)).all()
     return teams
