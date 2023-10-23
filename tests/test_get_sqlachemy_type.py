fd816bf0672f279395f112134d3b8cab285d39f7
Sebastián Ramírez <tiangolo@gmail.com>
2023-10-23 10:18:35 +0400

    🚚 Rename get_sqlalchemy_type

--- tests/test_get_sqlachemy_type.py
+++ tests/test_get_sqlachemy_type.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 
 import pytest
 from pydantic.fields import ModelField
-from sqlmodel.main import get_sqlachemy_type
+from sqlmodel.main import get_sqlalchemy_type
 
 
 @pytest.mark.parametrize(
@@ -17,4 +17,4 @@ from sqlmodel.main import get_sqlachemy_type
 def test_non_type_does_not_break(input_type: type) -> None:
     model_field_mock = MagicMock(ModelField, type_=input_type)
     with pytest.raises(ValueError):
-        get_sqlachemy_type(model_field_mock)
+        get_sqlalchemy_type(model_field_mock)
