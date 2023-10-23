42aa921abb9b59285a094944a7de1cdb60a7c58b
Sebastián Ramírez <tiangolo@gmail.com>
2023-10-23 11:25:23 +0400

    🎨 Format with Ruff

--- sqlmodel/__init__.py
+++ sqlmodel/__init__.py
@@ -5,12 +5,12 @@ from sqlalchemy.engine import create_mock_engine as create_mock_engine
 from sqlalchemy.engine import engine_from_config as engine_from_config
 from sqlalchemy.inspection import inspect as inspect
 from sqlalchemy.schema import BLANK_SCHEMA as BLANK_SCHEMA
+from sqlalchemy.schema import DDL as DDL
 from sqlalchemy.schema import CheckConstraint as CheckConstraint
 from sqlalchemy.schema import Column as Column
 from sqlalchemy.schema import ColumnDefault as ColumnDefault
 from sqlalchemy.schema import Computed as Computed
 from sqlalchemy.schema import Constraint as Constraint
-from sqlalchemy.schema import DDL as DDL
 from sqlalchemy.schema import DefaultClause as DefaultClause
 from sqlalchemy.schema import FetchedValue as FetchedValue
 from sqlalchemy.schema import ForeignKey as ForeignKey
@@ -23,6 +23,14 @@ from sqlalchemy.schema import Sequence as Sequence
 from sqlalchemy.schema import Table as Table
 from sqlalchemy.schema import ThreadLocalMetaData as ThreadLocalMetaData
 from sqlalchemy.schema import UniqueConstraint as UniqueConstraint
+from sqlalchemy.sql import LABEL_STYLE_DEFAULT as LABEL_STYLE_DEFAULT
+from sqlalchemy.sql import (
+    LABEL_STYLE_DISAMBIGUATE_ONLY as LABEL_STYLE_DISAMBIGUATE_ONLY,
+)
+from sqlalchemy.sql import LABEL_STYLE_NONE as LABEL_STYLE_NONE
+from sqlalchemy.sql import (
+    LABEL_STYLE_TABLENAME_PLUS_COL as LABEL_STYLE_TABLENAME_PLUS_COL,
+)
 from sqlalchemy.sql import alias as alias
 from sqlalchemy.sql import all_ as all_
 from sqlalchemy.sql import and_ as and_
@@ -48,14 +56,6 @@ from sqlalchemy.sql import insert as insert
 from sqlalchemy.sql import intersect as intersect
 from sqlalchemy.sql import intersect_all as intersect_all
 from sqlalchemy.sql import join as join
-from sqlalchemy.sql import LABEL_STYLE_DEFAULT as LABEL_STYLE_DEFAULT
-from sqlalchemy.sql import (
-    LABEL_STYLE_DISAMBIGUATE_ONLY as LABEL_STYLE_DISAMBIGUATE_ONLY,
-)
-from sqlalchemy.sql import LABEL_STYLE_NONE as LABEL_STYLE_NONE
-from sqlalchemy.sql import (
-    LABEL_STYLE_TABLENAME_PLUS_COL as LABEL_STYLE_TABLENAME_PLUS_COL,
-)
 from sqlalchemy.sql import lambda_stmt as lambda_stmt
 from sqlalchemy.sql import lateral as lateral
 from sqlalchemy.sql import literal as literal
@@ -85,55 +85,53 @@ from sqlalchemy.sql import values as values
 from sqlalchemy.sql import within_group as within_group
 from sqlalchemy.types import ARRAY as ARRAY
 from sqlalchemy.types import BIGINT as BIGINT
-from sqlalchemy.types import BigInteger as BigInteger
 from sqlalchemy.types import BINARY as BINARY
 from sqlalchemy.types import BLOB as BLOB
 from sqlalchemy.types import BOOLEAN as BOOLEAN
-from sqlalchemy.types import Boolean as Boolean
 from sqlalchemy.types import CHAR as CHAR
 from sqlalchemy.types import CLOB as CLOB
 from sqlalchemy.types import DATE as DATE
-from sqlalchemy.types import Date as Date
 from sqlalchemy.types import DATETIME as DATETIME
-from sqlalchemy.types import DateTime as DateTime
 from sqlalchemy.types import DECIMAL as DECIMAL
-from sqlalchemy.types import Enum as Enum
 from sqlalchemy.types import FLOAT as FLOAT
-from sqlalchemy.types import Float as Float
 from sqlalchemy.types import INT as INT
 from sqlalchemy.types import INTEGER as INTEGER
-from sqlalchemy.types import Integer as Integer
-from sqlalchemy.types import Interval as Interval
 from sqlalchemy.types import JSON as JSON
-from sqlalchemy.types import LargeBinary as LargeBinary
 from sqlalchemy.types import NCHAR as NCHAR
 from sqlalchemy.types import NUMERIC as NUMERIC
-from sqlalchemy.types import Numeric as Numeric
 from sqlalchemy.types import NVARCHAR as NVARCHAR
-from sqlalchemy.types import PickleType as PickleType
 from sqlalchemy.types import REAL as REAL
 from sqlalchemy.types import SMALLINT as SMALLINT
+from sqlalchemy.types import TEXT as TEXT
+from sqlalchemy.types import TIME as TIME
+from sqlalchemy.types import TIMESTAMP as TIMESTAMP
+from sqlalchemy.types import VARBINARY as VARBINARY
+from sqlalchemy.types import VARCHAR as VARCHAR
+from sqlalchemy.types import BigInteger as BigInteger
+from sqlalchemy.types import Boolean as Boolean
+from sqlalchemy.types import Date as Date
+from sqlalchemy.types import DateTime as DateTime
+from sqlalchemy.types import Enum as Enum
+from sqlalchemy.types import Float as Float
+from sqlalchemy.types import Integer as Integer
+from sqlalchemy.types import Interval as Interval
+from sqlalchemy.types import LargeBinary as LargeBinary
+from sqlalchemy.types import Numeric as Numeric
+from sqlalchemy.types import PickleType as PickleType
 from sqlalchemy.types import SmallInteger as SmallInteger
 from sqlalchemy.types import String as String
-from sqlalchemy.types import TEXT as TEXT
 from sqlalchemy.types import Text as Text
-from sqlalchemy.types import TIME as TIME
 from sqlalchemy.types import Time as Time
-from sqlalchemy.types import TIMESTAMP as TIMESTAMP
 from sqlalchemy.types import TypeDecorator as TypeDecorator
 from sqlalchemy.types import Unicode as Unicode
 from sqlalchemy.types import UnicodeText as UnicodeText
-from sqlalchemy.types import VARBINARY as VARBINARY
-from sqlalchemy.types import VARCHAR as VARCHAR
 
-# Extensions and modifications of SQLAlchemy in SQLModel
+# From SQLModel, modifications of SQLAlchemy or equivalents of Pydantic
 from .engine.create import create_engine as create_engine
-from .orm.session import Session as Session
-from .sql.expression import select as select
-from .sql.expression import col as col
-from .sql.sqltypes import AutoString as AutoString
-
-# Export SQLModel specifics (equivalent to Pydantic)
-from .main import SQLModel as SQLModel
 from .main import Field as Field
 from .main import Relationship as Relationship
+from .main import SQLModel as SQLModel
+from .orm.session import Session as Session
+from .sql.expression import col as col
+from .sql.expression import select as select
+from .sql.sqltypes import AutoString as AutoString
