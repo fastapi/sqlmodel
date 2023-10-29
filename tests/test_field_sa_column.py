00b30c65c9dc12c723ad28d54ebaca034e35d68d
Sebastián Ramírez <tiangolo@gmail.com>
2023-10-29 12:00:58 +0400

    ✅ Add test to ensure sa_type is not passed with sa_column

--- tests/test_field_sa_column.py
+++ tests/test_field_sa_column.py
@@ -39,6 +39,17 @@ def test_sa_column_no_sa_kargs() -> None:
             )
 
 
+def test_sa_column_no_type() -> None:
+    with pytest.raises(RuntimeError):
+
+        class Item(SQLModel, table=True):
+            id: Optional[int] = Field(
+                default=None,
+                sa_type=Integer,
+                sa_column=Column(Integer, primary_key=True),
+            )
+
+
 def test_sa_column_no_primary_key() -> None:
     with pytest.raises(RuntimeError):
 
