dbaef533dc66618d2b4d8ca0426907ebd750d04c
Sebastián Ramírez <tiangolo@gmail.com>
2023-11-25 11:25:39 +0100

    🚚 Move get_sqlalchemy_type and get_column_from_field back to main

--- sqlmodel/compat.py
+++ sqlmodel/compat.py
@@ -262,62 +262,6 @@ def _is_field_noneable(field: "FieldInfo") -> bool:
         return field.allow_none
 
 
-def get_sqlalchemy_type(field: Any) -> Any:
-    if IS_PYDANTIC_V2:
-        field_info = field
-    else:
-        field_info = field.field_info
-    sa_type = getattr(field_info, "sa_type", Undefined)  # noqa: B009
-    if sa_type is not Undefined:
-        return sa_type
-
-    type_ = get_type_from_field(field)
-    metadata = get_field_metadata(field)
-
-    # Check enums first as an enum can also be a str, needed by Pydantic/FastAPI
-    if issubclass(type_, Enum):
-        return sa_Enum(type_)
-    if issubclass(type_, str):
-        max_length = getattr(metadata, "max_length", None)
-        if max_length:
-            return AutoString(length=max_length)
-        return AutoString
-    if issubclass(type_, float):
-        return Float
-    if issubclass(type_, bool):
-        return Boolean
-    if issubclass(type_, int):
-        return Integer
-    if issubclass(type_, datetime):
-        return DateTime
-    if issubclass(type_, date):
-        return Date
-    if issubclass(type_, timedelta):
-        return Interval
-    if issubclass(type_, time):
-        return Time
-    if issubclass(type_, bytes):
-        return LargeBinary
-    if issubclass(type_, Decimal):
-        return Numeric(
-            precision=getattr(metadata, "max_digits", None),
-            scale=getattr(metadata, "decimal_places", None),
-        )
-    if issubclass(type_, ipaddress.IPv4Address):
-        return AutoString
-    if issubclass(type_, ipaddress.IPv4Network):
-        return AutoString
-    if issubclass(type_, ipaddress.IPv6Address):
-        return AutoString
-    if issubclass(type_, ipaddress.IPv6Network):
-        return AutoString
-    if issubclass(type_, Path):
-        return AutoString
-    if issubclass(type_, uuid.UUID):
-        return GUID
-    raise ValueError(f"{type_} has no matching SQLAlchemy type")
-
-
 def get_type_from_field(field: Any) -> type:
     if IS_PYDANTIC_V2:
         type_: type | None = field.annotation
@@ -365,60 +309,6 @@ def get_field_metadata(field: Any) -> Any:
         metadata.max_digits = getattr(field.type_, "max_digits", None)
         metadata.decimal_places = getattr(field.type_, "decimal_places", None)
         return metadata
-
-
-def get_column_from_field(field: Any) -> Column:  # type: ignore
-    if IS_PYDANTIC_V2:
-        field_info = field
-    else:
-        field_info = field.field_info
-    sa_column = getattr(field_info, "sa_column", Undefined)
-    if isinstance(sa_column, Column):
-        return sa_column
-    sa_type = get_sqlalchemy_type(field)
-    primary_key = getattr(field_info, "primary_key", Undefined)
-    if primary_key is Undefined:
-        primary_key = False
-    index = getattr(field_info, "index", Undefined)
-    if index is Undefined:
-        index = False
-    nullable = not primary_key and _is_field_noneable(field)
-    # Override derived nullability if the nullable property is set explicitly
-    # on the field
-    field_nullable = getattr(field_info, "nullable", Undefined)  # noqa: B009
-    if field_nullable is not Undefined:
-        assert not isinstance(field_nullable, UndefinedType)
-        nullable = field_nullable
-    args = []
-    foreign_key = getattr(field_info, "foreign_key", Undefined)
-    if foreign_key is Undefined:
-        foreign_key = None
-    unique = getattr(field_info, "unique", Undefined)
-    if unique is Undefined:
-        unique = False
-    if foreign_key:
-        assert isinstance(foreign_key, str)
-        args.append(ForeignKey(foreign_key))
-    kwargs = {
-        "primary_key": primary_key,
-        "nullable": nullable,
-        "index": index,
-        "unique": unique,
-    }
-    sa_default = Undefined
-    if field_info.default_factory:
-        sa_default = field_info.default_factory
-    elif field_info.default is not Undefined:
-        sa_default = field_info.default
-    if sa_default is not Undefined:
-        kwargs["default"] = sa_default
-    sa_column_args = getattr(field_info, "sa_column_args", Undefined)
-    if sa_column_args is not Undefined:
-        args.extend(list(cast(Sequence[Any], sa_column_args)))
-    sa_column_kwargs = getattr(field_info, "sa_column_kwargs", Undefined)
-    if sa_column_kwargs is not Undefined:
-        kwargs.update(cast(Dict[Any, Any], sa_column_kwargs))
-    return Column(sa_type, *args, **kwargs)  # type: ignore
 
 
 def post_init_field_info(field_info: FieldInfo) -> None:
