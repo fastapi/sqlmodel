b733fe194d7b636c9e37f2d340df6ffddcc05cd5
Sebastián Ramírez <tiangolo@gmail.com>
2023-11-26 18:48:44 +0100

    ✅ Add tests for new source examples for Python 3.9 and 3.10, still needs pytest markers

---
+++ tests/test_tutorial/test_code_structure/test_tutorial002_py310.py
@@ -0,0 +1,37 @@
+from unittest.mock import patch
+
+from sqlmodel import create_engine
+
+from ...conftest import get_testing_print_function
+
+expected_calls = [
+    [
+        "Created hero:",
+        {
+            "id": 1,
+            "name": "Deadpond",
+            "age": None,
+            "secret_name": "Dive Wilson",
+            "team_id": 1,
+        },
+    ],
+    [
+        "Hero's team:",
+        {"name": "Z-Force", "headquarters": "Sister Margaret’s Bar", "id": 1},
+    ],
+]
+
+
+def test_tutorial(clear_sqlmodel):
+    from docs_src.tutorial.code_structure.tutorial002_py310 import app, database
+
+    database.sqlite_url = "sqlite://"
+    database.engine = create_engine(database.sqlite_url)
+    app.engine = database.engine
+    calls = []
+
+    new_print = get_testing_print_function(calls)
+
+    with patch("builtins.print", new=new_print):
+        app.main()
+    assert calls == expected_calls
