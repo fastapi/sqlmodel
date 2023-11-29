cb02fb2e66a69a899c5556f78f5fca14d8991f63
Sebastián Ramírez <tiangolo@gmail.com>
2023-11-29 19:41:04 +0100

    🐛 Fix detection of unions, detected by new Python 3.10 tests

--- sqlmodel/compat.py
+++ sqlmodel/compat.py
@@ -1,4 +1,4 @@
-from types import NoneType
+import types
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -38,7 +38,8 @@ else:
 if TYPE_CHECKING:
     from .main import RelationshipInfo, SQLModel
 
-
+UnionType = getattr(types, "UnionType", Union)
+NoneType = type(None)
 T = TypeVar("T")
 InstanceOrType = Union[T, Type[T]]
 
@@ -53,6 +54,10 @@ else:
     class SQLModelConfig(BaseConfig):
         table: Optional[bool] = None
         registry: Optional[Any] = None
+
+
+def _is_union_type(t: Any) -> bool:
+    return t is UnionType or t is Union
 
 
 def get_config_value(
@@ -147,8 +152,22 @@ def get_relationship_to(
             else:
                 return use_annotation
         # If Union (e.g. Optional), get the real field
-        elif origin is Union:
-            use_annotation = get_args(annotation)[0]
+        elif _is_union_type(origin):
+            use_annotation = get_args(annotation)
+            if len(use_annotation) > 2:
+                raise ValueError(
+                    "Cannot have a (non-optional) union as a SQLAlchemy field"
+                )
+            arg1, arg2 = use_annotation
+            if arg1 is NoneType and arg2 is not NoneType:
+                use_annotation = arg2
+            elif arg2 is NoneType and arg1 is not NoneType:
+                use_annotation = arg1
+            else:
+                raise ValueError(
+                    "Cannot have a Union of None and None as a SQLAlchemy field"
+                )
+
         # If a list, then also get the real field
         elif origin is list:
             use_annotation = get_args(annotation)[0]
@@ -206,7 +225,7 @@ def _is_field_noneable(field: "FieldInfo") -> bool:
                 return False
             if field.annotation is None or field.annotation is NoneType:
                 return True
-            if get_origin(field.annotation) is Union:
+            if _is_union_type(get_origin(field.annotation)):
                 for base in get_args(field.annotation):
                     if base is NoneType:
                         return True
@@ -230,7 +249,7 @@ def get_type_from_field(field: Any) -> type:
         origin = get_origin(type_)
         if origin is None:
             return type_
-        if origin is Union:
+        if _is_union_type(origin):
             bases = get_args(type_)
             if len(bases) > 2:
                 raise ValueError(
