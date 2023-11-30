9bb29aafb5fc84cd2de36139ad1ce10553738c5f
Sebastián Ramírez <tiangolo@gmail.com>
2023-11-30 17:12:58 +0100

    ✅ Simplify test that shoudln't require Pydantic v1 or v2

--- tests/test_instance_no_args.py
+++ tests/test_instance_no_args.py
@@ -6,18 +6,12 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from sqlmodel import Field, SQLModel
 
-from .conftest import needs_pydanticv1, needs_pydanticv2
 
-
-@needs_pydanticv1
 def test_allow_instantiation_without_arguments_pydantic_v1(clear_sqlmodel):
-    class Item(SQLModel):
+    class Item(SQLModel, table=True):
         id: Optional[int] = Field(default=None, primary_key=True)
         name: str
         description: Optional[str] = None
-
-        class Config:
-            table = True
 
     engine = create_engine("sqlite:///:memory:")
     SQLModel.metadata.create_all(engine)
@@ -40,23 +34,3 @@ def test_not_allow_instantiation_without_arguments_if_not_table():
 
     with pytest.raises(ValidationError):
         Item()
-
-
-@needs_pydanticv2
-def test_allow_instantiation_without_arguments_pydnatic_v2(clear_sqlmodel):
-    class Item(SQLModel, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
-        name: str
-        description: Optional[str] = None
-
-    engine = create_engine("sqlite:///:memory:")
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as db:
-        item = Item()
-        item.name = "Rick"
-        db.add(item)
-        db.commit()
-        result = db.execute(select(Item)).scalars().all()
-    assert len(result) == 1
-    assert isinstance(item.id, int)
-    SQLModel.metadata.clear()
