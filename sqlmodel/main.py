be49ad5f9215959471fe687dc712508c57b5b449
Sebastián Ramírez <tiangolo@gmail.com>
2023-12-04 14:54:40 +0100

    ✨ Add override for .dict() only to deprecate it and help people migrate to model_dump, even (if) before they migrate to Pydantic v2

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -794,6 +794,31 @@ class SQLModel(BaseModel, metaclass=SQLModelMetaclass, registry=default_registry
                 exclude_none=exclude_none,
             )
 
+    @deprecated(
+        """
+        🚨 `obj.dict()` was deprecated in SQLModel 0.0.14, you should
+        instead use `obj.model_dump()`.
+        """
+    )
+    def dict(
+        self,
+        *,
+        include: IncEx = None,
+        exclude: IncEx = None,
+        by_alias: bool = False,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> Dict[str, Any]:
+        return self.model_dump(
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+        )
+
     @classmethod
     @deprecated(
         """
