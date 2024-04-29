a37655f26679c5745922992993139fe542858f2e
Sebastián Ramírez <tiangolo@gmail.com>
2024-04-29 15:06:14 -0700

    🐛 Fix passing Pydantic parameters for version 2.7 only in 2.7

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -781,7 +781,7 @@ class SQLModel(BaseModel, metaclass=SQLModelMetaclass, registry=default_registry
         warnings: Union[bool, Literal["none", "warn", "error"]] = True,
         serialize_as_any: bool = False,
     ) -> Dict[str, Any]:
-        if PYDANTIC_VERSION < "2.7.0":
+        if PYDANTIC_VERSION >= "2.7.0":
             extra_kwargs: Dict[str, Any] = {
                 "context": context,
                 "serialize_as_any": serialize_as_any,
