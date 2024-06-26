94295beffecf52cbebf9d590a3b6afae766a0ea7
Esteban Maya Cadavid <emayacadavid9@gmail.com>
2024-06-25 22:07:28 -0500

    🧪 Add test for UUIDs field definition and support

---
+++ tests/test_advanced/test_uuid/test_tutorial001.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from sqlmodel import create_engine
+
+from ...conftest import get_testing_print_function
+
+expected_calls = [
+    ["Found item: Example Item"],
+    ["Item deleted successfully."],
+]
+
+
+def test_tutorial(clear_sqlmodel):
+    from docs_src.advanced.uuid import tutorial001 as mod
+
+    mod.sqlite_url = "sqlite://"
+    mod.engine = create_engine(mod.sqlite_url)
+    calls = []
+
+    new_print = get_testing_print_function(calls)
+
+    with patch("builtins.print", new=new_print):
+        mod.main()
+    assert calls == expected_calls
