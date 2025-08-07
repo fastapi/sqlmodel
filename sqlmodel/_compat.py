8e149c83016b4fd4fd8651e7e3d2534e953ff7dd
nsaccente <nsaccente@users.noreply.github.com>
2025-08-07 19:21:14 -0400

    Test patch for literal

--- sqlmodel/_compat.py
+++ sqlmodel/_compat.py
@@ -10,6 +10,7 @@ from typing import (
     Dict,
     ForwardRef,
     Generator,
+    Literal,
     Mapping,
     Optional,
     Set,
@@ -22,6 +23,7 @@ from pydantic import VERSION as P_VERSION
 from pydantic import BaseModel
 from pydantic.fields import FieldInfo
 from typing_extensions import Annotated, get_args, get_origin
+from .sql.sqltypes import AutoString
 
 # Reassign variable to make it reexported for mypy
 PYDANTIC_VERSION = P_VERSION
