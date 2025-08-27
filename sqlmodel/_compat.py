ab774b5d15200e51b4bc53091a6a2b507cb504e0
Sofie Van Landeghem <svlandeg@users.noreply.github.com>
2025-08-27 12:23:32 +0200

    commit suggestion by Yurii
    Co-authored-by: Motov Yurii <109919500+YuriiMotov@users.noreply.github.com>

--- sqlmodel/_compat.py
+++ sqlmodel/_compat.py
@@ -462,8 +462,8 @@ else:
 
     def get_sa_type_from_field(field: Any) -> Any:
         if get_origin(field.type_) is Literal:
-            return AutoString
-        elif isinstance(field.type_, type) and field.shape == SHAPE_SINGLETON:
+            return Literal
+        if isinstance(field.type_, type) and field.shape == SHAPE_SINGLETON:
             return field.type_
         raise ValueError(f"The field {field.name} has no matching SQLAlchemy type")
 
