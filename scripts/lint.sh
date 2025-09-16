74113b9a160979bd57e4a94f277a160068b1e807
svlandeg <svlandeg@github.com>
2025-09-16 19:15:17 +0200

    expand on mypy command to debug CI failure

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy sqlmodel
+mypy --no-incremental --show-absolute-path ./sqlmodel
 ruff check sqlmodel tests docs_src scripts
 ruff format sqlmodel tests docs_src scripts --check
