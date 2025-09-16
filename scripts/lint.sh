94d0cb308454811360ebf8caaa128e4755168539
svlandeg <svlandeg@github.com>
2025-09-16 20:51:12 +0200

    experiment with from future import

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-mypy --no-incremental --show-absolute-path ./sqlmodel
+mypy sqlmodel
 ruff check sqlmodel tests docs_src scripts
 ruff format sqlmodel tests docs_src scripts --check
