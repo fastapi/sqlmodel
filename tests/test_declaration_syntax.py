93e15c1b88813e9bfa00525161769a788c8673a6
Julio C. Galindo <54072664+stickM4N@users.noreply.github.com>
2025-10-11 05:27:23 +0200

    Add tests for syntax declaration

---
+++ tests/test_declaration_syntax.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from sqlmodel import SQLModel, Field
+
+
+def test_declaration_syntax_1():
+    class Person1(SQLModel, table=True):
+        name: str = Field(primary_key=True)
+
+
+def test_declaration_syntax_2():
+    class Person2(SQLModel, table=True):
+        name: Annotated[str, Field(primary_key=True)]
+
+
+def test_declaration_syntax_3():
+    class Person3(SQLModel, table=True):
+        name: Annotated[str, ...] = Field(primary_key=True)
