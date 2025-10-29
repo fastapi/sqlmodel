d128318acf5c54387471863a2b4d9a825a8cefb3
Julio C. Galindo <54072664+stickM4N@users.noreply.github.com>
2025-10-29 13:12:35 +0100

    📝 Add needs_pydanticv2 decorator to tests for Pydantic v2 compatibility

--- tests/test_field_sa_type.py
+++ tests/test_field_sa_type.py
@@ -5,7 +5,7 @@ import pytest
 import typing_extensions as te
 from sqlmodel import Field, SQLModel
 
-from tests.conftest import needs_py312
+from tests.conftest import needs_py312, needs_pydanticv2
 
 
 def test_sa_type_typing_1() -> None:
@@ -44,6 +44,7 @@ if hasattr(t, "TypeAlias"):
 
 
 @needs_py312
+@needs_pydanticv2
 def test_sa_type_typing_5() -> None:
     test_code = dedent("""
     type Type5_t = str
@@ -56,6 +57,7 @@ def test_sa_type_typing_5() -> None:
 
 
 @needs_py312
+@needs_pydanticv2
 def test_sa_type_typing_6() -> None:
     test_code = dedent("""
     type Type6_t = t.Annotated[str, "Just a comment"]
@@ -129,6 +131,7 @@ if hasattr(te, "TypeAlias"):
 
 
 @needs_py312
+@needs_pydanticv2
 def test_sa_type_typing_extensions_5() -> None:
     test_code = dedent("""
     type Type5_te = str
@@ -141,6 +144,7 @@ def test_sa_type_typing_extensions_5() -> None:
 
 
 @needs_py312
+@needs_pydanticv2
 def test_sa_type_typing_extensions_6() -> None:
     test_code = dedent("""
     type Type6_te = te.Annotated[str, "Just a comment"]
@@ -152,6 +156,7 @@ def test_sa_type_typing_extensions_6() -> None:
     exec(test_code, globals())
 
 
+@needs_pydanticv2
 def test_sa_type_typing_extensions_7() -> None:
     Type7_te = te.NewType("Type7_te", str)
 
