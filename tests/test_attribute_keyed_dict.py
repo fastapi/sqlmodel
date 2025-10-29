3f11b80d792ab609ffa3c12916c294784934a35f
Natarajan Krishnaswami <n_krishnaswami@apple.com>
2025-10-29 16:39:51 -0400

    Make dict get_relationship_to more robust

--- tests/test_attribute_keyed_dict.py
+++ tests/test_attribute_keyed_dict.py
@@ -1,6 +1,8 @@
+import re
 from enum import Enum
 from typing import Dict, Optional
 
+import pytest
 from sqlalchemy.orm.collections import attribute_keyed_dict
 from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
 
@@ -43,3 +45,46 @@ def test_attribute_keyed_dict_works(clear_sqlmodel):
         assert parent.children_by_color[Color.Blue].parent_id == parent.id
         assert parent.children_by_color[Color.Blue].color == Color.Blue
         assert parent.children_by_color[Color.Blue].value == 2
+
+
+def test_dict_relationship_throws_on_missing_annotation_arg(clear_sqlmodel):
+    class Color(str, Enum):
+        Orange = "Orange"
+        Blue = "Blue"
+
+    class Child(SQLModel, table=True):
+        __tablename__ = "children"
+
+        id: Optional[int] = Field(primary_key=True, default=None)
+        parent_id: int = Field(foreign_key="parents.id")
+        color: Color
+        value: int
+
+    error_msg_re = re.escape(
+        "Dict/Mapping relationship field 'children_by_color' must have both key and value type arguments (e.g., dict[str, Model])"
+    )
+    # No type args
+    with pytest.raises(ValueError, match=error_msg_re):
+
+        class Parent(SQLModel, table=True):
+            __tablename__ = "parents"
+
+            id: Optional[int] = Field(primary_key=True, default=None)
+            children_by_color: dict[()] = Relationship(
+                sa_relationship_kwargs={
+                    "collection_class": attribute_keyed_dict("color")
+                }
+            )
+
+    # One type arg
+    with pytest.raises(ValueError, match=error_msg_re):
+
+        class Parent(SQLModel, table=True):
+            __tablename__ = "parents"
+
+            id: Optional[int] = Field(primary_key=True, default=None)
+            children_by_color: dict[Color] = Relationship(
+                sa_relationship_kwargs={
+                    "collection_class": attribute_keyed_dict("color")
+                }
+            )
