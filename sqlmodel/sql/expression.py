a1ad013eb3f3298b11312de9b8c473fd0c147cf1
kakeruzoku <kakeruzoku@gmail.com>
2025-11-07 20:58:57 +0900

    🐛 Fix `tuple_` type hinting mistake

--- sqlmodel/sql/expression.py
+++ sqlmodel/sql/expression.py
@@ -192,8 +192,8 @@ def over(
 def tuple_(
     *clauses: Union[_ColumnExpressionArgument[Any], Any],
     types: Optional[Sequence["_TypeEngineArgument[Any]"]] = None,
-) -> Tuple[Any, ...]:
-    return sqlalchemy.tuple_(*clauses, types=types)  # type: ignore[return-value]
+) -> sqlalchemy.Tuple[Any, ...]:
+    return sqlalchemy.tuple_(*clauses, types=types)
 
 
 def type_coerce(
