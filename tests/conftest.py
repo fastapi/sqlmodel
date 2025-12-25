eec65c6e89077c165be1a4b22795fb89c4cccff0
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-25 11:51:05 +0100

    Remove `needs_py39` marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -89,7 +89,6 @@ def print_mock_fixture() -> Generator[PrintMock, None, None]:
 needs_pydanticv2 = pytest.mark.skipif(not IS_PYDANTIC_V2, reason="requires Pydantic v2")
 needs_pydanticv1 = pytest.mark.skipif(IS_PYDANTIC_V2, reason="requires Pydantic v1")
 
-needs_py39 = pytest.mark.skipif(sys.version_info < (3, 9), reason="requires python3.9+")
 needs_py310 = pytest.mark.skipif(
     sys.version_info < (3, 10), reason="requires python3.10+"
 )
