05af4de9af5ddeb8c57a5e05632703e7b3eddd65
Yurii Motov <yurii.motov.monte@gmail.com>
2025-12-25 10:57:23 +0100

    Fix copying coverage file in `cov_tmp_path` fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,7 +31,7 @@ def clear_sqlmodel() -> Any:
 def cov_tmp_path(tmp_path: Path) -> Generator[Path, None, None]:
     yield tmp_path
     for coverage_path in tmp_path.glob(".coverage*"):
-        coverage_destiny_path = top_level_path / coverage_path.name
+        coverage_destiny_path = top_level_path / "coverage" / coverage_path.name
         shutil.copy(coverage_path, coverage_destiny_path)
 
 
