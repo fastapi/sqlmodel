d9fc682ab03cc9b1334c562efcfd2654d41cd39c
Yurii Motov <yurii.motov.monte@gmail.com>
2026-02-04 23:34:14 +0100

    Update `ValidationError` schema

--- tests/test_tutorial/test_fastapi/test_response_model/test_tutorial001.py
+++ tests/test_tutorial/test_fastapi/test_response_model/test_tutorial001.py
@@ -2,6 +2,7 @@ import importlib
 from types import ModuleType
 
 import pytest
+from dirty_equals import IsOneOf
 from fastapi.testclient import TestClient
 from sqlmodel import create_engine
 from sqlmodel.pool import StaticPool
@@ -146,17 +147,38 @@ def test_tutorial(module: ModuleType):
                         "title": "ValidationError",
                         "required": ["loc", "msg", "type"],
                         "type": "object",
-                        "properties": {
-                            "loc": {
-                                "title": "Location",
-                                "type": "array",
-                                "items": {
-                                    "anyOf": [{"type": "string"}, {"type": "integer"}]
+                        "properties": IsOneOf(
+                            {
+                                "loc": {
+                                    "title": "Location",
+                                    "type": "array",
+                                    "items": {
+                                        "anyOf": [
+                                            {"type": "string"},
+                                            {"type": "integer"},
+                                        ]
+                                    },
                                 },
+                                "msg": {"title": "Message", "type": "string"},
+                                "type": {"title": "Error Type", "type": "string"},
                             },
-                            "msg": {"title": "Message", "type": "string"},
-                            "type": {"title": "Error Type", "type": "string"},
-                        },
+                            {
+                                "loc": {
+                                    "title": "Location",
+                                    "type": "array",
+                                    "items": {
+                                        "anyOf": [
+                                            {"type": "string"},
+                                            {"type": "integer"},
+                                        ]
+                                    },
+                                },
+                                "msg": {"title": "Message", "type": "string"},
+                                "type": {"title": "Error Type", "type": "string"},
+                                "ctx": {"title": "Context", "type": "object"},
+                                "input": {"title": "Input"},
+                            },
+                        ),
                     },
                 }
             },
