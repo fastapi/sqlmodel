533189410b273205d60e8c572b849eb0024d639b
Yurii Motov <yurii.motov.monte@gmail.com>
2026-02-21 08:32:10 +0100

    Fix `Optional` to Python 3.10+ syntax in test

--- tests/test_field_sa_fk_args_kwargs.py
+++ tests/test_field_sa_fk_args_kwargs.py
@@ -1,23 +1,21 @@
-from typing import Optional
-
 from sqlalchemy import ForeignKey, create_engine
 from sqlmodel import Field, SQLModel
 
 
 def test_base_model_fk(clear_sqlmodel, caplog) -> None:
     class User(SQLModel, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     class Base(SQLModel):
-        owner_id: Optional[int] = Field(
+        owner_id: int | None = Field(
             default=None, sa_column_args=(ForeignKey("user.id", ondelete="SET NULL"),)
         )
 
     class Asset(Base, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     class Document(Base, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     engine = create_engine("sqlite://", echo=True)
     SQLModel.metadata.create_all(engine)
@@ -29,19 +27,19 @@ def test_base_model_fk(clear_sqlmodel, caplog) -> None:
 
 def test_base_model_fk_args(clear_sqlmodel, caplog) -> None:
     class User(SQLModel, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     class Base(SQLModel):
-        owner_id: Optional[int] = Field(
+        owner_id: int | None = Field(
             default=None,
             foreign_key=ForeignKey("user.id", ondelete="SET NULL"),
         )
 
     class Asset(Base, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     class Document(Base, table=True):
-        id: Optional[int] = Field(default=None, primary_key=True)
+        id: int | None = Field(default=None, primary_key=True)
 
     engine = create_engine("sqlite://", echo=True)
     SQLModel.metadata.create_all(engine)
