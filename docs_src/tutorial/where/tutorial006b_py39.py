8047fcf5e5002f30a1ab30c59527e26b5564fb96
svlandeg <svlandeg@github.com>
2026-03-18 17:32:07 +0100

    cleanup

--- docs_src/tutorial/where/tutorial006b_py39.py
+++ docs_src/tutorial/where/tutorial006b_py39.py
@@ -1,13 +1,11 @@
-from typing import Optional
-
 from sqlmodel import Field, Session, SQLModel, col, create_engine, select
 
 
 class Hero(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str
     secret_name: str
-    age: Optional[int] = None
+    age: int | None = None
 
 
 sqlite_file_name = "database.db"
