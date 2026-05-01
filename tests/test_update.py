4885b991444b6b927957cfeb68cb55e7f2793cf2
A00474880 <haodong.tao@smu.ca>
2026-05-01 15:18:41 -0300

    add model_dump options to sqlmodel_update

--- tests/test_update.py
+++ tests/test_update.py
@@ -1,3 +1,4 @@
+from pytest import raises
 from sqlmodel import Field, SQLModel
 
 
@@ -5,10 +6,12 @@ def test_sqlmodel_update():
     class Organization(SQLModel, table=True):
         id: int = Field(default=None, primary_key=True)
         name: str
+        city: str
         headquarters: str
 
     class OrganizationUpdate(SQLModel):
         name: str
+        city: str | None = None
 
     org = Organization(name="Example Org", city="New York", headquarters="NYC HQ")
     org_in = OrganizationUpdate(name="Updated org")
@@ -17,4 +20,13 @@ def test_sqlmodel_update():
         update={
             "headquarters": "-",  # This field is in Organization, but not in OrganizationUpdate
         },
+        exclude_unset=True
     )
+    # fields that should stay the same
+    assert org.city == "New York"
+    #fields that should be updated
+    assert org.name == "Updated org"
+    assert org.headquarters == "-"
+    # test raise value error when passing in updates other than dict or BaseModel
+    with raises(ValueError):
+        org.sqlmodel_update(["Boston"])
