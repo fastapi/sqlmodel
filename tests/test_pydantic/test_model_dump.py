d8c14f6eb405d82de462e4f91bda0149e8fb4c6b
Yurii Motov <yurii.motov.monte@gmail.com>
2026-05-26 15:18:57 +0200

    Simplify test via parameterization

--- tests/test_pydantic/test_model_dump.py
+++ tests/test_pydantic/test_model_dump.py
@@ -1,9 +1,28 @@
+from typing import Any
+
 import pytest
 from sqlmodel import SQLModel
 from sqlmodel._compat import PYDANTIC_MINOR_VERSION
 
 
-def test_polymorphic_serialization_default():
+@pytest.mark.parametrize(
+    ("polymorphic_serialization", "expected_result"),
+    [
+        (None, {"user": {"name": "pydantic"}}),
+        (False, {"user": {"name": "pydantic"}}),
+        pytest.param(
+            True,
+            {"user": {"name": "pydantic", "password": "password"}},
+            marks=pytest.mark.skipif(
+                PYDANTIC_MINOR_VERSION < (2, 13),
+                reason="polymorphic_serialization is only available in Pydantic v2.13+",
+            ),
+        ),
+    ],
+)
+def test_polymorphic_serialization(
+    polymorphic_serialization: bool | None, expected_result: dict[str, Any]
+):
 
     class User(SQLModel):
         name: str
@@ -18,48 +37,11 @@ def test_polymorphic_serialization_default():
         user=UserLogin(name="pydantic", password="password"),
     )
 
-    assert outer_model.model_dump() == {"user": {"name": "pydantic"}}
-
-
-def test_polymorphic_serialization_false():
-
-    class User(SQLModel):
-        name: str
-
-    class UserLogin(User):
-        password: str
-
-    class OuterModel(SQLModel):
-        user: User
-
-    outer_model = OuterModel(
-        user=UserLogin(name="pydantic", password="password"),
+    assert (
+        outer_model.model_dump(polymorphic_serialization=polymorphic_serialization)
+        == expected_result
     )
 
     assert outer_model.model_dump(polymorphic_serialization=False) == {
         "user": {"name": "pydantic"}
-    }
-
-
-@pytest.mark.skipif(
-    PYDANTIC_MINOR_VERSION < (2, 13),
-    reason="polymorphic_serialization is only available in Pydantic v2.13+",
-)
-def test_polymorphic_serialization_true():
-
-    class User(SQLModel):
-        name: str
-
-    class UserLogin(User):
-        password: str
-
-    class OuterModel(SQLModel):
-        user: User
-
-    outer_model = OuterModel(
-        user=UserLogin(name="pydantic", password="password"),
-    )
-
-    assert outer_model.model_dump(polymorphic_serialization=True) == {
-        "user": {"name": "pydantic", "password": "password"}
     }
