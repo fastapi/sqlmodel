80618e2ba0f99652889baff5d0600a7d22343ae3
Yurii Motov <yurii.motov.monte@gmail.com>
2026-05-26 15:26:51 +0200

    Revert "Simplify test via parameterization"
    This reverts commit f414deed6980378ffbd582286a92dc661f58b8e4.

--- tests/test_pydantic/test_model_dump.py
+++ tests/test_pydantic/test_model_dump.py
@@ -1,20 +1,7 @@
-from typing import Any
-
-import pytest
 from sqlmodel import SQLModel
 
 
-@pytest.mark.parametrize(
-    ("polymorphic_serialization", "expected_result"),
-    [
-        (None, {"user": {"name": "pydantic"}}),
-        (False, {"user": {"name": "pydantic"}}),
-        (True, {"user": {"name": "pydantic", "password": "password"}}),
-    ],
-)
-def test_polymorphic_serialization(
-    polymorphic_serialization: bool | None, expected_result: dict[str, Any]
-):
+def test_polymorphic_serialization_default():
 
     class User(SQLModel):
         name: str
@@ -29,7 +16,44 @@ def test_polymorphic_serialization(
         user=UserLogin(name="pydantic", password="password"),
     )
 
-    assert (
-        outer_model.model_dump(polymorphic_serialization=polymorphic_serialization)
-        == expected_result
+    assert outer_model.model_dump() == {"user": {"name": "pydantic"}}
+
+
+def test_polymorphic_serialization_false():
+
+    class User(SQLModel):
+        name: str
+
+    class UserLogin(User):
+        password: str
+
+    class OuterModel(SQLModel):
+        user: User
+
+    outer_model = OuterModel(
+        user=UserLogin(name="pydantic", password="password"),
     )
+
+    assert outer_model.model_dump(polymorphic_serialization=False) == {
+        "user": {"name": "pydantic"}
+    }
+
+
+def test_polymorphic_serialization_true():
+
+    class User(SQLModel):
+        name: str
+
+    class UserLogin(User):
+        password: str
+
+    class OuterModel(SQLModel):
+        user: User
+
+    outer_model = OuterModel(
+        user=UserLogin(name="pydantic", password="password"),
+    )
+
+    assert outer_model.model_dump(polymorphic_serialization=True) == {
+        "user": {"name": "pydantic", "password": "password"}
+    }
